d62862b2deaad8f0c3dd30f8e37ed995da14459b
danuri <danualbantani@gmail.com>
2023-06-09 17:06:21 +0700

    Update Auth.php

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -16,7 +16,7 @@ class Auth extends BaseController
     public function login()
     {
       $agent = $this->request->getUserAgent();
-      if(str_contains($agent,'')){
+      if(str_contains($agent,'ukompenyuluh')){
         $user = $this->request->getVar('nik');
         $nopes = $this->request->getVar('nomor_peserta');
         $pass = $this->request->getVar('nik');
