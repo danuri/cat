c8ec4fd58920d4b7c8b9201b52c639b6f63cbc8e
edwi.ropeg <edwi.ropeg@kemenag.go.id>
2024-06-21 15:53:58 +0700

    fix link CAT Kemenag

--- app/Views/template.php
+++ app/Views/template.php
@@ -36,7 +36,7 @@
             <div class="d-flex">
                 <!-- LOGO -->
                 <div class="navbar-brand-box horizontal-logo">
-                    <a href="index.html" class="logo logo-dark">
+                    <a href="cat" class="logo logo-dark">
                         <span class="logo-sm">
                             <img src="<?= base_url()?>xassets/images/logo-sm.png" alt="" height="22">
                         </span>
@@ -45,7 +45,7 @@
                         </span>
                     </a>
 
-                    <a href="index.html" class="logo logo-light">
+                    <a href="cat" class="logo logo-light">
                         <span class="logo-sm">
                             <img src="<?= base_url()?>xassets/images/logo-sm.png" alt="" height="22">
                         </span>
